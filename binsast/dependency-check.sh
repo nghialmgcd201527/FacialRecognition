f51c501cc269ad81b41defc8b4cd4ec403b7fb8c
Minh Nghia <lmnghia911@gmail.com>
2024-01-11 15:43:58 +0700

    update bin

---
+++ binsast/dependency-check.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+DC_VERSION="latest"
+DC_PROJECT="dependency-check scan: $(pwd)"
+
+# Make sure we are using the latest version
+docker pull public.ecr.aws/govtechsg/cicd-images:dependency-check-latest
+
+docker run --rm \
+    -e user=$USER \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    --volume $(pwd):/src:z \
+    --volume $(pwd)/odc-reports:/report:z \
+    public.ecr.aws/govtechsg/cicd-images:dependency-check-latest \
+    --scan /src \
+    --format JUNIT \
+    --format HTML \
+    --nodeAuditSkipDevDependencies \
+    --nodePackageSkipDevDependencies \
+    --cveValidForHours 24 \
+    --failOnCVSS 7 \
+    --junitFailOnCVSS 3 \
+    --project "%DC_PROJECT%" \
+    --out /report
+    # Use suppression like this: (where /src == $pwd)
+    # --suppression "/src/security/dependency-check-suppression.xml"
