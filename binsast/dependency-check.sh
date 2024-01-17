f59d7db548165964b94eaae6dae809387623ac21
Minh Nghia <lmnghia911@gmail.com>
2024-01-17 13:54:43 +0700

    fix

--- binsast/dependency-check.sh
+++ binsast/dependency-check.sh
@@ -1,26 +1,51 @@
+# #!/bin/sh
+
+# DC_VERSION="latest"
+# DC_PROJECT=`jq -r ".name" package.json`
+
+# # Make sure we are using the latest version
+# docker pull public.ecr.aws/govtechsg/cicd-images:dependency-check-latest
+
+# docker run --rm \
+#     -e user=$USER \
+#     -u $(id -u ${USER}):$(id -g ${USER}) \
+#     --volume $(pwd):/src:z \
+#     --volume $(pwd)/odc-reports:/report:z \
+#     public.ecr.aws/govtechsg/cicd-images:dependency-check-latest \
+#     --scan /src \
+#     --format JUNIT \
+#     --format HTML \
+#     --format JSON \
+#     --nodeAuditSkipDevDependencies \
+#     --nodePackageSkipDevDependencies \
+#     --nvdValidForHours 24 \
+#     --failOnCVSS 7 \
+#     --junitFailOnCVSS 3 \
+#     --project "$DC_PROJECT" \
+#     --out /report
+#     # Use suppression like this: (where /src == $pwd)
+#     # --suppression "/src/security/dependency-check-suppression.xml"
+
 #!/bin/sh
 
 DC_VERSION="latest"
-DC_PROJECT=`jq -r ".name" package.json`
+DC_DIRECTORY=$HOME/OWASP-Dependency-Check
+DC_PROJECT="dependency-check scan: $(pwd)"
+DATA_DIRECTORY="$DC_DIRECTORY/data"
+CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
 
 # Make sure we are using the latest version
-docker pull public.ecr.aws/govtechsg/cicd-images:dependency-check-latest
+docker pull owasp/dependency-check:$DC_VERSION
 
 docker run --rm \
     -e user=$USER \
     -u $(id -u ${USER}):$(id -g ${USER}) \
     --volume $(pwd):/src:z \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
     --volume $(pwd)/odc-reports:/report:z \
-    public.ecr.aws/govtechsg/cicd-images:dependency-check-latest \
+    owasp/dependency-check:$DC_VERSION \
     --scan /src \
-    --format JUNIT \
-    --format HTML \
-    --format JSON \
-    --nodeAuditSkipDevDependencies \
-    --nodePackageSkipDevDependencies \
-    --nvdValidForHours 24 \
-    --failOnCVSS 7 \
-    --junitFailOnCVSS 3 \
+    --format "ALL" \
     --project "$DC_PROJECT" \
     --out /report
     # Use suppression like this: (where /src == $pwd)
